274ecdb947b19a01405e474011cc661132c9b235
unknown <hemogem@gmail.com>
2024-07-27 14:56:34 -0600

    Nuevos cambios 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
+import {useState, useEffect} from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from "./data/db"
 
 function App() {
 
-  return (
-    <>
-        <Header />
-        <main className="container-xl mt-5">
-            <h2 className="text-center">Nuestra Colección</h2>
+    const [data, setData] = useState([])
+    const [cart, setCart] = useState([])
 
-            <div className="row mt-5">          
-                <Guitar />
-            </div>
-        </main>
+    useEffect(()=> {
+        setData(db)
+    }, [])
 
-        <footer className="bg-dark mt-5 py-5">
-            <div className="container-xl">
-                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-            </div>
-        </footer>
-    </>
-  )
+    function addToCart(item){
+
+        const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
+        console.log(itemExists)
+
+        setCart(prevCart => [...prevCart, item])
+    }
+
+    return (
+        <>
+            <Header />
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">          
+                    {data.map((guitar) => (
+                        <Guitar
+                            key={guitar.id} 
+                            guitar={guitar}
+                            addToCart={addToCart}
+                        />                        
+                    ))}                    
+                </div>
+            </main>
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default App
